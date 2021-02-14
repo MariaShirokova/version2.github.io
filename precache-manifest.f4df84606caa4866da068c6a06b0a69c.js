7d38a76d00073f2532e6e35542a516b2554fc568
MariaShirokova <55069158+MariaShirokova@users.noreply.github.com>
2021-02-14 17:13:00 -0500

    changes
    1402

---
+++ precache-manifest.f4df84606caa4866da068c6a06b0a69c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a31e377fb8dcaed09b7b35745cf9f678",
+    "url": "./index.html"
+  },
+  {
+    "revision": "79c5f2c9882e780deada",
+    "url": "./static/css/2.e5de342f.chunk.css"
+  },
+  {
+    "revision": "b0a768adafbbd1480255",
+    "url": "./static/css/main.e9365824.chunk.css"
+  },
+  {
+    "revision": "79c5f2c9882e780deada",
+    "url": "./static/js/2.f5adc56d.chunk.js"
+  },
+  {
+    "revision": "d54fad59c1eb3ece728f8281ec580cd6",
+    "url": "./static/js/2.f5adc56d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c45dc47082e0f9839e",
+    "url": "./static/js/3.4d8bc539.chunk.js"
+  },
+  {
+    "revision": "b0a768adafbbd1480255",
+    "url": "./static/js/main.4545d7e8.chunk.js"
+  },
+  {
+    "revision": "00dc717ba29d6f3e983e",
+    "url": "./static/js/runtime-main.9bc257df.js"
+  },
+  {
+    "revision": "4291733509f3aa56eb797bcf5ca831d7",
+    "url": "./static/media/alpha-background.42917335.svg"
+  },
+  {
+    "revision": "3828cde44093f9cecd2f9ff4a1d0fc97",
+    "url": "./static/media/blob.3828cde4.svg"
+  },
+  {
+    "revision": "cca01512e1b467eb33cecf9220eaf678",
+    "url": "./static/media/ellipse.cca01512.svg"
+  },
+  {
+    "revision": "8848c01ba23c5f9ae57a9f9da4d96908",
+    "url": "./static/media/play.8848c01b.svg"
+  },
+  {
+    "revision": "6be0083d7bf7bacf68ccde131d3df58c",
+    "url": "./static/media/polygon.6be0083d.svg"
+  },
+  {
+    "revision": "796cbb722fc49b62bb6129b838747eab",
+    "url": "./static/media/rectangle.796cbb72.svg"
+  },
+  {
+    "revision": "6813ed741c2e8b8ba73727eb6bd24267",
+    "url": "./static/media/save.6813ed74.svg"
+  },
+  {
+    "revision": "16c4cbfbdd6c3450f8650926c93b1cfc",
+    "url": "./static/media/upload.16c4cbfb.svg"
+  }
+]);
